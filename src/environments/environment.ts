b860cfbdfa953b8517079a41198eb071d0779a43
monilpshah <monil31199@gmail.com>
2019-04-28 10:42:25 +0530

    done

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -20,8 +20,8 @@ export const environment ={
   production:false
 };
 export const url={
-  endPoint:'http://godaddykalpataru.herokuapp.com/'
-  // endPoint:'http://localhost:3000/'
+  // endPoint:'http://godaddykalpataru.herokuapp.com/'
+  endPoint:'http://localhost:3000/'
 }
 /*
  * For easier debugging in development mode, you can import the following file
